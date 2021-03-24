c9138ac8b143087380b8f40e602e28b3556304bd
James Leslie <jamesleslie314@gmail.com>
2021-03-24 10:10:31 -0400

    Added literate agda to blog.

--- other/agda-html-insert.hs
+++ other/agda-html-insert.hs
@@ -1,22 +1,16 @@
-import System.Environment
 import System.Directory
 import System.IO
 import Data.List
 
 str :: String
-str = "---\nlayout: agda\n---\n" -- The text to prefix the files with.
+str = "#+SETUPFILE: /home/james/Dropbox/Documents/jeslie0.github.io/public/css/org-html-themes/org/theme-readtheorg-local.setup\n"  -- The text to prefix the files with.
 
 folderLocation :: String
-folderLocation = "/home/james/Dropbox/Documents/jeslie0.github.io/UniAgda/" -- The location of the folder of files to prefix. Must end in "/"
+folderLocation = "/home/james/Dropbox/Documents/Agda/Univalent-Agda/org/" -- The location of the folder of files to prefix. Must end in "/"
 
   
 inserter :: String -> String
 inserter xs = str ++ xs
-
-otherSubList :: Integer -> [b] -> [b]
-otherSubList _ [] = []
-otherSubList 0 list = list
-otherSubList n (_:xs) = otherSubList (n-1) xs
 
 fileInserter :: FilePath -> IO ()
 fileInserter path = do
@@ -33,7 +27,7 @@ fileInserter path = do
 
 main = do
   lst_of_files_in_folder <- getDirectoryContents folderLocation
-  let lst_of_files = map (\xs -> folderLocation ++ xs) $ delete "." $ delete ".." lst_of_files_in_folder
+  let lst_of_files = map (\xs -> folderLocation ++ xs) $ delete "." $ delete ".." $ delete "ltximg" lst_of_files_in_folder
   sequence $ map fileInserter lst_of_files
 
 
